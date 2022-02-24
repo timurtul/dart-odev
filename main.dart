48fa9de6ffea89628eb15d5d54d8d4f2008451a9
Timur Tül <38362206+timurtul@users.noreply.github.com>
2022-02-25 00:12:53 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,32 @@
+// 5 Tane Default Function
+
+defaultFunc([String name = "Timur"]){
+  return print(' Adım $name ');
+}
+
+myData([yasim= 15]) {
+ return print('Yaşım $yasim'); 
+}
+
+personalData(ad, soyad, departman, [yas= 30]) {
+ return print('Adım $ad Soyadım $soyad Çalıştığım Departman $departman Yaşım $yas'); 
+}
+
+turkey(nufus, gelir, gider[kurulusyili= 1923]) {
+ return print('Türkiye nin nüfusu $nufus yıllık geliri $gelir TL gideri ise $gider TL kuruluş yılı $kurulusyili '); 
+}
+
+firma([yasim= 15]) {
+ return print('Yaşım $yasim'); 
+}
+
+
+//5 Tane Named Function
+
+//5 Tane Required Function
+
+//Material IO 2 Widget Tanıtım
+
+//Cascades'e kadar
+//https://dart.dev/codelabs/dart-cheatsheet#cascades
 
